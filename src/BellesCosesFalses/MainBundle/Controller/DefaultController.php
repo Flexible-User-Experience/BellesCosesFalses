2b422e78afd479ee9451ec8797125298a0db2e48
David Romaní <david@flux.cat>
2013-05-20 15:48:11 +0200

    step 03.8 - sitemap added

--- src/BellesCosesFalses/MainBundle/Controller/DefaultController.php
+++ src/BellesCosesFalses/MainBundle/Controller/DefaultController.php
@@ -10,4 +10,21 @@ class DefaultController extends Controller
     {
         return $this->render('BellesCosesFalsesMainBundle:Default:index.html.twig');
     }
+
+    public function sitemapAction()
+    {
+        $urls = array();
+        $hostname = $this->getRequest()->getHost();
+        array_push($urls, array('loc' => $this->get('router')->generate('home'), 'changefreq' => 'weekly', 'priority' => '1.0'));
+        array_push($urls, array('loc' => $this->get('router')->generate('exposicio'), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('apartats'), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('artistes'), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('agraiments'), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('credits'), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('blog'), 'changefreq' => 'daily', 'priority' => '1.0'));
+        return $this->render('FluxPageBundle:Global:sitemap.xml.twig', array(
+            'urls' => $urls,
+            'hostname' => $hostname,
+        ));
+    }
 }
