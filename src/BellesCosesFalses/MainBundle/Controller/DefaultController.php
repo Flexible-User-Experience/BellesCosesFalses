c2e3496f15ca4abb55aa8072a4cb00b60e16ae93
David Romaní <david@flux.cat>
2013-05-21 10:42:15 +0200

    step 04.8 - provisional postal

--- src/BellesCosesFalses/MainBundle/Controller/DefaultController.php
+++ src/BellesCosesFalses/MainBundle/Controller/DefaultController.php
@@ -8,7 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BellesCosesFalsesMainBundle:Default:index.html.twig');
+        return $this->render('BellesCosesFalsesMainBundle:Default:provisional.html.twig'); // PROVISIONAL - TEMPORALMENT ACTIVAT
+        // return $this->render('BellesCosesFalsesMainBundle:Default:index.html.twig'); DEFINITIU - TEMPORALMENT DESACTIVAT
     }
 
     public function sitemapAction()
