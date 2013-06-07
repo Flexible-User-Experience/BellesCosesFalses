360b6e09772d81206e8109ff092dbcdda1d0b32e
David Romaní <david@flux.cat>
2013-06-07 17:47:58 +0200

    step 10.0 - layout enhacements

--- src/BellesCosesFalses/MainBundle/Menu/Builder.php
+++ src/BellesCosesFalses/MainBundle/Menu/Builder.php
@@ -12,11 +12,11 @@ class Builder extends ContainerAware
         $menu = $factory->createItem('root');
         $menu->setCurrentUri($this->container->get('request')->getRequestUri());
         $menu->addChild('EXPOSICIÓ', array('route' => 'exposicio'));
-        $menu->addChild('APARTATS', array('route' => 'apartats'));
+        //$menu->addChild('APARTATS', array('route' => 'apartats'));
         $menu->addChild('ARTISTES', array('route' => 'artistes'));
-        $menu->addChild('CONCURS INSTAGRAM', array('route' => 'agraiments'));
-        $menu->addChild('CRÈDITS I AGRAÏMENTS', array('route' => 'credits'));
         $menu->addChild('BLOG', array('route' => 'blog'));
+        $menu->addChild('CONCURS', array('route' => 'agraiments'));
+        $menu->addChild('CRÈDITS', array('route' => 'credits'));
         return $menu;
     }
 }
