e5171a8def357536070896bacca5b23a6297cd09
David Romaní <david@flux.cat>
2013-07-11 18:39:32 +0200

    step 12.9 - blog submenu added

--- src/BellesCosesFalses/MainBundle/Menu/Builder.php
+++ src/BellesCosesFalses/MainBundle/Menu/Builder.php
@@ -9,12 +9,22 @@ class Builder extends ContainerAware
 {
     public function main_menu(FactoryInterface $factory, array $options)
     {
+        $em = $this->container->get('doctrine')->getManager();
+        $posts = $em->getRepository('FluxBlogBundle:Post')->getAllActivePostsSortedByDate();
         $menu = $factory->createItem('root');
         $menu->setCurrentUri($this->container->get('request')->getRequestUri());
         $menu->addChild('EXPOSICIÓ', array('route' => 'exposicio'));
         //$menu->addChild('APARTATS', array('route' => 'apartats'));
         //$menu->addChild('ARTISTES', array('route' => 'artistes'));
-        $menu->addChild('BLOG', array('route' => 'blog'));
+        $blog = $menu->addChild('BLOG', array('route' => 'blog'));
+        foreach ($posts as $post) {
+            $blog->addChild($post->getTitle(), array('route' => 'blog_detail', 'routeParameters' => array(
+                'id' => 1,
+                'year' => 2,
+                'month' => 3,
+                'day' => 4,
+                'titleslug' => 's')));
+        }
         $menu->addChild('CONCURS', array('route' => 'agraiments'));
         $menu->addChild('CRÈDITS', array('route' => 'credits'));
         return $menu;
